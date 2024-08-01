fb3e20cd20db93b298329de80fee72f207503af9
gecolonr <gecolonr@berkeley.edu>
2024-08-01 13:04:46 +0200

    added code to change gen portfolio and gfm to gfl ratio. need to verify.

--- experiments/g_test_plot.jl
+++ experiments/g_test_plot.jl
@@ -1,21 +1,28 @@
+using TLmodels
 path = "data/gab_tests/results0.jls"
-
 df = load_serde_data(path::String)
+
+include("plotting.jl")
 
 p = makeplots(
     df;
 	
     rows="Line Model",
-    # cols="Injector Setup",
-    color="ZIPE Load Params",
+    # color="ZIPE Parameters",
     slider="Power Setpoint",
     slider_current_value_prefix="Power Setpoint: ",
 	
     x=(x0=0.48, dx=0.00005),
-    y="Bus 3 Inj Current",
+    y="Load Voltage at Bus 5",
     x_title=L"\mathrm{Time}\:\: [\mathrm{s}]",
     y_title = L"\mathrm{Current}\:\:[\mathrm{p.u.}]",
 	
     supertitle="Bus 3 Injector Current",
     yaxis_home_range = (min=0, max=10),
 )
+
+savehtmlplot(p, "data/gab_tests/plot0.html")
+
+x = df[!,"Load Voltage at Bus 5"]
+using Plots
+plot(t, x)
